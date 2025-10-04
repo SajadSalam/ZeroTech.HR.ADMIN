246067fcb2b8e837510dcf8695d5f5ba48efc614
Sajad Salam <sjadsalam123@gmail.com>
2025-10-05 00:49:45 +0300

    Clean up

--- views/applicants/components/charts/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DailyLineChart } from './DailyLineChart.vue'
-export { default as GenderPieChart } from './GenderPieChart.vue'
-export { default as HepiqBarChart } from './HepiqBarChart.vue'
-
