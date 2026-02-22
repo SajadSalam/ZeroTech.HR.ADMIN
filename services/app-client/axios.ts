6eefc51a5995430d55679ee94483f84ba7218ba8
Abdulrahman <Abdulrahman.Khalil@ta3leem.iq>
2026-02-22 14:30:20 +0300

    Update axios base URL for API requests: change from previous server IP address to a new one for improved connectivity.

--- services/app-client/axios.ts
+++ services/app-client/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useToast } from '~/composables/toaster'
 
 // export const baseURL = 'https://hr-api.pomelo-bot.xyz/'
-export const baseURL = 'http://157.245.67.219:5001/'
+export const baseURL = 'http://104.248.85.137:5001/'
 // export const baseURL = import.meta.env.VITE_BASE_URL
 
 const axiosIns = axios.create({
